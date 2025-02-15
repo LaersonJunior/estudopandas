fafac582dec1a66f057da385445877a3fbe2d92c
Laerson Junior <laersontamorim@gmail.com>
2025-02-15 00:13:38 -0300

    Teste para criação de dataframe

--- pandas_1.py
+++ pandas_1.py
@@ -10,3 +10,13 @@ data = pd.read_csv('D:\GitHub\EstudoPandas\.venv\dataset\GasPricesinBrazil_2004-
 
 # Corrigido para acessar corretamente as dimensões
 print(f'O Dataframe possui {data.shape[0]} linhas/observações/registros e {data.shape[1]} colunas/variáveis/atributos')
+
+
+personagens_df = pd.DataFrame({
+    'nome':['Luke SkyWalker','Yoda','Palpatine'],
+    'Idade':['16','1000','70'],
+    'peso':['70','15','60'],
+    'eh jedi':[True,True,False] #O nome da coluna pode ter espaço
+})
+
+print(personagens_df.info())
