4978f09a81c2ae4ec720cf563849ab5d14df3102
Laerson Junior <laersontamorim@gmail.com>
2025-02-15 00:20:44 -0300

    refactor pandas_1.py to rename dataframe columns and clean up commented code

--- pandas_1.py
+++ pandas_1.py
@@ -8,15 +8,25 @@ data = pd.read_csv('D:\GitHub\EstudoPandas\.venv\dataset\GasPricesinBrazil_2004-
 #       data.head(),
 #       data.isnull().sum())
 
-# Corrigido para acessar corretamente as dimensões
-print(f'O Dataframe possui {data.shape[0]} linhas/observações/registros e {data.shape[1]} colunas/variáveis/atributos')
+# # Corrigido para acessar corretamente as dimensões
+# print(f'O Dataframe possui {data.shape[0]} linhas/observações/registros e {data.shape[1]} colunas/variáveis/atributos')
 
 
 personagens_df = pd.DataFrame({
     'nome':['Luke SkyWalker','Yoda','Palpatine'],
-    'Idade':['16','1000','70'],
+    'idade':['16','1000','70'],
     'peso':['70','15','60'],
     'eh jedi':[True,True,False] #O nome da coluna pode ter espaço
 })
 
-print(personagens_df.info())
+# print(personagens_df.info())
+
+# Renoemando as colunas
+presonagens_df_renomeado = personagens_df.rename(columns={'nome':'Nome Completo','idade':'Idade'})
+
+# Renomear direto no dataframe
+personagens_df.rename(columns={
+    'nome':'Nome Completo',
+    'idade':'Idade'},inplace=True)
+
+print(personagens_df)
